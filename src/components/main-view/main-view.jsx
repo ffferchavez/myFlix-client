79566dbaf12343f19f92023987f41797d0d93771
ffferchavez <138360629+ffferchavez@users.noreply.github.com>
2024-06-11 09:47:46 +0200

    created a loginvew and signupview components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,27 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
 
   useEffect(() => {
-    fetch("https://marvel-flix-c3644575f8db.herokuapp.com/movies")
+    if (!token) return;
+
+    fetch("https://marvel-flix-c3644575f8db.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
-      .then((data) => {
+      .then((movies) => {
         setMovies(
-          data.map((movie) => ({
+          movies.map((movie) => ({
             id: movie._id,
             title: movie.Title,
             description: movie.Description,
@@ -22,7 +32,24 @@ const MainView = () => {
           }))
         );
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem("token", token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   const getSimilarMovies = (movies, currentMovie) => {
     return movies.filter(
@@ -58,11 +85,37 @@ const MainView = () => {
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <div>
+        <nav>
+          <button
+            onClick={() => {
+              setUser(null);
+              setToken(null);
+              localStorage.clear();
+            }}
+          >
+            Logout
+          </button>
+        </nav>
+        The list is empty!
+      </div>
+    );
   }
 
   return (
     <div>
+      <nav>
+        <button
+          onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+          }}
+        >
+          Logout
+        </button>
+      </nav>
       {movies.map((movie) => (
         <MovieCard
           key={movie._id}
