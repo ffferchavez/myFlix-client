83aa6936ad12265a6b1692d8860b1720b98ecdd0
ffferchavez <138360629+ffferchavez@users.noreply.github.com>
2024-06-12 10:09:05 +0200

    fixed bugs with dependencies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,42 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 
 const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      description:
-        "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea in the mind of a CEO.",
-      genre: "Action, Science Fiction",
-      director: "Christopher Nolan",
-      imageUrl:
-        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-    },
-    {
-      id: 2,
-      title: "The Shawshank Redemption",
-      description:
-        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-      genre: "Drama",
-      director: "Frank Darabont",
-      imageUrl: "https://example.com/shawshank-redemption-poster.jpg",
-    },
-    {
-      id: 3,
-      title: "The Godfather",
-      description:
-        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-      genre: "Crime, Drama",
-      director: "Francis Ford Coppola",
-      imageUrl: "https://example.com/the-godfather-poster.jpg",
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetch("https://marvel-flix-c3644575f8db.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+        setMovies(
+          movies.map((movie) => ({
+            id: movie._id,
+            title: movie.Title,
+            description: movie.Description,
+            genre: movie.Genre,
+            director: movie.Director,
+            imagePath: movie.ImagePath,
+            featured: movie.Featured,
+          }))
+        );
+      });
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem("token", token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
+
+  const getSimilarMovies = (movies, currentMovie) => {
+    return movies.filter(
+      (movie) => movie.genre.Name === currentMovie.genre.Name
+    );
+  };
 
   if (selectedMovie) {
     const similarMovies = getSimilarMovies(movies, selectedMovie);
@@ -101,8 +120,8 @@ const MainView = () => {
         <MovieCard
           key={movie._id}
           movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
+          onMovieClick={() => {
+            setSelectedMovie(movie);
           }}
         />
       ))}
