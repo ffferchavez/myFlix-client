4b3e73c3d310b0558c84ab7e6ccdc31cb7d19f22
ffferchavez <138360629+ffferchavez@users.noreply.github.com>
2024-06-12 09:34:54 +0200

    Revert "updated mainview and moviecard code"
    This reverts commit ab12ec61de4505fd0a4635028563895d4c7b2cbb.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,38 +1,11 @@
-import PropTypes from "prop-types";
-import { Card } from "react-bootstrap";
-
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <Card onClick={() => onMovieClick(movie)} style={{ cursor: "pointer" }}>
-      <Card.Img variant="top" src={movie.ImagePath} alt={movie.Title} />
-      <Card.Body>
-        <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Description}</Card.Text>
-        <Card.Text>Genre: {movie.Genre?.Name || "N/A"}</Card.Text>
-        <Card.Text>Director: {movie.Director?.Name || "N/A"}</Card.Text>
-        <Card.Text>Featured: {movie.Featured ? "Yes" : "No"}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
   );
-};
-
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
-    }).isRequired,
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string,
-      Death: PropTypes.string,
-    }).isRequired,
-    Featured: PropTypes.bool.isRequired,
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
